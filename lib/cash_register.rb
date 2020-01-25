883d2c6364b754ae3c59f5a42411bd1591ccbb20
Eryk Rachwal <erykrachwal@yahoo.com>
2020-01-25 22:33:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,48 +1,38 @@
-class CashRegister
-  attr_accessor :total, :discount, :price, :items, :last_transaction, :title
-
-  def initialize(discount=0) 
-    @items = []
-    @total = 0 
+class CashRegister 
+  attr_accessor :total, :discount, :items, :last_transaction
+  
+  
+  def initialize(discount = 0)
+    @total = 0
     @discount = discount
-  end
-  
-  def total
-    @total
-  end   
-  
-  def add_item(title,price,quantity=1)
-    @title = title
-    @price = price
-    @total += price * quantity
-    @last_transaction = @total
-  
-    if quantity > 1 
-       i = 0 
-      while i < quantity 
-       @items << title
-       i += 1
-     end
-   else
-    @items << title
-    end
-     @last_transaction = @total
-    @total
+    @items = []
   end 
   
+  def total 
+    @total 
+  end 
+
+  def add_item(title, price, quantity = 1) 
+    quantity == 1 ? self.total += price : self.total += price * quantity
+    @items.fill(title, @items.length , quantity)
+    @last_transaction = price * quantity
+  end 
+    
   def apply_discount
-    if discount > 0 
-    new_price = (price*discount)/100
-    @total -= new_price
-    "After the discount, the total comes to $#{@total}."
+    if discount == 0 
+      return "There is no discount to apply."
     else 
-    "There is no discount to apply."
-    end 
-
-  end
-
+      total_disc = ((discount.to_f/100) * total).to_i
+      self.total = total.to_i - total_disc
+      return "After the discount, the total comes to $#{self.total}."
+    end
+  end 
+  
+  def items 
+    @items
+  end 
+  
   def void_last_transaction
-    @total -= @price 
-    @last_transaction = 0.00  
-  end
-end
+    @total = @total - @last_transaction
+  end 
+end 
